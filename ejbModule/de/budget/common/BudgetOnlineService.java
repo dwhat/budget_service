48385c84d7d5b7f23631dade9b83004047693762
marco135 <mkuepers@gmx.de>
2015-05-08 16:32:55 +0200

    Interface hinzugefügt und login udn logout eingefügt

---
+++ ejbModule/de/budget/common/BudgetOnlineService.java
@@ -0,0 +1,32 @@
+package de.budget.common;
+
+
+/*
+ * Interface BudgetOnlineService
+ * @author Marco
+ * @date 08.05.2015
+ */
+public interface BudgetOnlineService {
+
+		
+	/**
+	* Method to login with Username and Password
+	* @param username
+	* @param password
+	* @return
+	* @author Marco
+	* @date 08.05.2015
+	*/
+	public boolean login(String username, String password);
+		
+	
+	/**
+	* Method to log out
+	* @throws NoSessionException
+	* @author Marco
+	* @date 08.05.2015
+	*/
+	public void logout();
+		
+
+}
