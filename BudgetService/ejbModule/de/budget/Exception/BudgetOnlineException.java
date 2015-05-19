3bdf5bbd6b7e23c188abb5200e36af3b40005da1
walli12 <moritz_wallersheim@web.de>
2015-05-19 20:13:09 +0200

    Logging erweitert.

--- BudgetService/ejbModule/de/budget/Exception/BudgetOnlineException.java
+++ BudgetService/ejbModule/de/budget/Exception/BudgetOnlineException.java
@@ -1,14 +1,21 @@
 package de.budget.Exception;
 
+import org.jboss.logging.Logger;
+
+
+
 public class BudgetOnlineException extends Exception {
 	
 	private static final long serialVersionUID = -1658425297634781761L;
+	
+	private static final Logger logger = Logger.getLogger(BudgetOnlineException.class);
 	
 	private int errorCode;
 	
 	public BudgetOnlineException(int errorCode, String message) {
 		super(message);
 		this.errorCode = errorCode;
+		logger.error("Fehlerfall. Code=" + errorCode + "Message=" + message);
 	}
 
 	public int getErrorCode() {
