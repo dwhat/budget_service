a811108627aeca5afe5c55effdfda4e41e9b2df4
marco135 <mkuepers@gmx.de>
2015-06-09 17:28:45 +0200

    Code von Thöne aus Client gelöscht

--- JavaSoapClient/src/de/budget/client/SimpleOnlineBudgetClient.java
+++ JavaSoapClient/src/de/budget/client/SimpleOnlineBudgetClient.java
@@ -38,10 +38,7 @@ public class SimpleOnlineBudgetClient {
  	       	System.out.println();
  	       
  	       	//Test-Szeanarien ausfuehren:
- 	       	//szenarioEmma();
- 	       	//szenarioJoe();		   	       
- 	       	//szenarioEmma();
- 	       	//szenarioPeter();
+ 	       	szenarioRegister();
  	       	szenarioCategory();
  	       	szenarioVendor();
  	       	szenarioPayment();
@@ -343,102 +340,26 @@ public class SimpleOnlineBudgetClient {
 		   System.out.println("Message: " + venResp.getMessage());
 		   System.out.println("============================================================");
     }
-	
-	
-    /**
-     * Test-Szenario: Emma meldet sich an
-     */
 
-	private static void szenarioEmma() {
-	   System.out.println("============================================================");
-       UserLoginResponse loginResponse = remoteSystem.login("emma", "12345678");
-       if (loginResponse != null & loginResponse.getReturnCode()==200) {
-    	   int sessionId = loginResponse.getSessionId();
-		   System.out.println("Emma hat sich angemeldet");
-		   System.out.println(loginResponse.getReturnCode());
-		   /*
-		   System.out.println("Emma legt ein Payment an");
-		   PaymentResponse paymentResponse = remoteSystem.createOrUpdatePayment(sessionId, 0, "Volksbank", "123456789", "DE12345678", true);
-		   System.out.println("Emma hat ein Payment angelegt");
-		   System.out.println("Paymentname = " + paymentResponse.getPaymentTo().getName());
-		   System.out.println("Paymentnumber = " + paymentResponse.getPaymentTo().getNumber());
-		   System.out.println("PaymentBIC = " + paymentResponse.getPaymentTo().getBic());
-		   System.out.println(paymentResponse.getReturnCode());
-		   */
-		   // Payment anlegen
-			PaymentResponse payResp = remoteSystem.createOrUpdatePayment(sessionId, 12, "Konto1789", "123456789", "BIC", true);
-			System.out.println("Neuerzeugtes Konto mit Namen: " + payResp.getReturnCode());
-			System.out.println("Neuerzeugtes Konto mit Namen: " + payResp.getPaymentTo().getName());
-			System.out.println("Joa hat ein Payment angelegt");
-					
-			// Payment abrufen
-			PaymentListResponse payListResp = remoteSystem.getPayments(sessionId);
-			ArrayList<PaymentTO> paymentList = (ArrayList<PaymentTO>) payListResp.getPaymentList();
-			System.out.println("Erster Name des Kontos: " + paymentList.get(0).getName());
-			
-			//Logout
-	       ReturnCodeResponse resp = remoteSystem.logout(sessionId);
-		   System.out.println("Emma hat sich abgemeldet.");
-		   System.out.println(resp.getReturnCode());
-		   
-       }
-       else {
-    	   System.out.println(loginResponse.getMessage());
-       }
-	}
 
-    /**
-     * Test-Szenario: Joe meldet sich an
-     */
-	/*
-	private static void szenarioJoe() {
-       UserLoginResponse loginResponse = remoteSystem.login("joe", "joe1");
-       if (loginResponse.getReturnCode()==0) {
-    	   int sessionId = loginResponse.getSessionId();
-		   System.out.println("Joe hat sich angemeldet, um eine Ueberweisung aufzugeben.");
-
-		   AccountListResponse accountListResponse = remoteSystem.getMyAccounts(sessionId); 
-	       List<AccountTO> joesKonten = accountListResponse.getAccountList();
-	       if (joesKonten.size()>0) {
-	    	   //gib Joes Konten aus:
-		       System.out.println("Joe hat folgende Konten:");
-		       for (AccountTO k : joesKonten) {
-		    	   System.out.println("Konto-Nr: " + k.getId() + " Saldo: " + k.getBalance());
-		       }
-		       System.out.println();
-		       
-		       TransferMoneyResponse transferResponse = remoteSystem.transfer(sessionId, JOES_KONTO, EMMAS_KONTO, new BigDecimal(33));
-		       System.out.println("Joe hat von Konto "+ JOES_KONTO +" 33 EUR an Konto " + EMMAS_KONTO + " ueberwiesen.");
-		       System.out.println("Der neue Saldo von Konto "+JOES_KONTO+" betraegt: " + transferResponse.getNewBalance());
-	       }
-	       remoteSystem.logout(sessionId);
-		   System.out.println("Joe hat sich abgemeldet.");
-	   }
-       else {
-    	   System.out.println(loginResponse.getMessage());
-       }       
-	}
-	*/
 	/**
 	 * Test-Szenario mit Kundenregistrierung 
 	 */
-	/*
-	private static void szenarioPeter() {
+	private static void szenarioRegister() {
 	   System.out.println();
 	   System.out.println("============================================================");
-	   System.out.println("Peters Szenario:");
+	   System.out.println("Register Szenario:");
 
-       UserLoginResponse loginResponse = remoteSystem.registerNewCustomer("peter", "peter1");
+       UserLoginResponse loginResponse = remoteSystem.setUser("emma", "12345678", "test@mail.com");
        if (loginResponse.getReturnCode()==0) {
     	   int sessionId = loginResponse.getSessionId();
-		   System.out.println("Peter hat sich erfolgreich registriert.");
+		   System.out.println("Emma hat sich erfolgreich registriert.");
 	       remoteSystem.logout(sessionId);
-		   System.out.println("Peter hat sich abgemeldet.");
+		   System.out.println("Emma hat sich abgemeldet.");
 	   }
        else {
     	   System.out.println(loginResponse.getMessage());
        }       
 	}
-	*/
 }
 
