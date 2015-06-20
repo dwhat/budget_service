4ae2f3f670fa19fe48375121252206e77c24824f
walli12 <moritz_wallersheim@web.de>
2015-06-20 12:32:19 +0200

    ExceptionHandling erweitert !

--- BudgetService/ejbModule/de/budget/Exception/BudgetOnlineException.java
+++ BudgetService/ejbModule/de/budget/Exception/BudgetOnlineException.java
@@ -30,23 +30,23 @@ public class BudgetOnlineException extends Exception {
 		case "SESSION_NOT_FOUND_EXCEPTION":
 			//this.errorCode = XYZ falls für android alles 404 sein muss. ErrorCode anpassung
 			this.errorMessage = "Bitte loggen Sie sich zunächst ein.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "USER_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Ihre Anmedung ist fehl geschlagen. Username nicht gefunden. Username = " + additionalMessage;
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "PASSWORD_INVALID_EXCEPTION":
 			this.errorMessage = "Ihr Passwort ist ungültig. Mindestens 8 höchstens 250 Zeichen.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.warn("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "PASSWORD_WRONG_EXCEPTION":
 			this.errorMessage = "Ihr Passwort ist falsch.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.warn("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;	
 		case "USER_EXISTS_EXCEPTION":
 			this.errorMessage = "Username existiert bereits.";
-			logger.error("");
+			logger.warn("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "ILLEGAL_ARGUMENT_EXCEPTION":
 			this.errorMessage = "Illegale Eingabe.";
@@ -58,27 +58,27 @@ public class BudgetOnlineException extends Exception {
 			break;
 		case "ITEM_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Item nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "INCOME_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Income nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "CATEGORY_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Kategorie nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "VENDOR_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Vendor nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "PAYMENT_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Payment nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "BASKET_NOT_FOUND_EXCEPTION":
 			this.errorMessage = "Basket nicht gefunden.";
-			logger.error("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
+			logger.info("BudgetOnline | Message: " + errorMessage + " | ExtraInfo: " + additionalMessage);
 			break;
 		case "ROLLBACK_EXCEPTION":
 			this.errorMessage = "Transaktion aufgrund von Referenzen nicht durchführbar.";
@@ -90,7 +90,7 @@ public class BudgetOnlineException extends Exception {
 			break;
 		default:
 			this.errorMessage = additionalMessage;
-			logger.error("BudgetOnline | Fatal Error!");
+			logger.error("BudgetOnline | Fatal Error! | Message: " + errorMessage);
 			break;
 		}
 		
